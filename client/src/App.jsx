a603840198049986ab0f83b78eedb232cc1c97d9
Ishan Roy <ishanroy3118107@gmail.com>
2025-08-25 16:43:49 +0530

    Minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,16 @@ import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Landing from "./pages/Landing";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   )

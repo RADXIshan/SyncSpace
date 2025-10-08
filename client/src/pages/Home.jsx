77452b3a8cf00bd81bcda31e57d98c9cb9c5e763
Ishan Roy <ishanroy3118107@gmail.com>
2025-10-09 00:53:01 +0530

    Minor reroute

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,6 +16,7 @@ const Home = () => {
           <Route path="/" element={<Navigate to="/home/dashboard" replace />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/calendar" element={<Calendar />} />
+         <Route path="*" element={<Navigate to="/home/dashboard" replace />} />
         </Routes>
         {showSettings && <Settings onClose={() => setShowSettings(false)} />}
       </main>

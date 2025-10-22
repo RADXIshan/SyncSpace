2a24187516f1c02b3ce0f6553a1bc4b5a32acabf
Vrinda Patnaik <patnaikvrinda@gmail.com>
2025-10-22 23:09:13 +0530

    feat(landing): add Features component to showcase problem-solution pairs

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -2,6 +2,7 @@ import FAQList from "../components/FAQList";
 import LandingNav  from "../components/LandingNav";
 import LandingHero from "../components/LandingHero";
 import LandingComparision from "../components/LandingComparison";
+import LandingFeatures from "../components/LandingFeatures";
 
 const Landing = () => {
   return (
@@ -9,6 +10,7 @@ const Landing = () => {
       <LandingNav />
       <LandingHero />
       <LandingComparision />
+      <LandingFeatures />
       <FAQList />
     </>
   );

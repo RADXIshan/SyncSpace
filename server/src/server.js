78dbb406a5593e84d7fd40a3137245413d7719ea
Ishan Roy <ishanroy3118107@gmail.com>
2025-10-28 18:39:03 +0530

    Minor change

--- server/src/server.js
+++ server/src/server.js
@@ -49,7 +49,7 @@ const allowedOrigins = [
 
 const corsOptions = {
   origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
+    
     if (!origin) return callback(null, true);
 
     if (allowedOrigins.indexOf(origin) !== -1) {
